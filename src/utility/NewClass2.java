4ddb51b571b0fb71e4b1144c8f050dbd6d0e65c4
Nurulla <numuminov@gmail.com>
2020-08-05 21:53:40 -0400

    add NewClass2.java

--- src/utility/NewClass2.java
+++ src/utility/NewClass2.java
@@ -4,5 +4,6 @@ public class NewClass2 {
     public static void main(String[] args) {
         System.out.println("Myself");
         System.out.println("Yourself");
+        System.out.println("bye");
     }
 }
